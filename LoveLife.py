95289fe21eb24e883686e77dc4f434f55d268ae9
inzaghibest <448435439@qq.com>
2016-07-19 19:27:25 +0800

    first modle

--- LoveLife.py
+++ LoveLife.py
@@ -4,29 +4,49 @@ import tornado.web
 import tornado.options
 import tornado.httpserver
 import tornado.ioloop
+import base64,uuid
+import yaml
 import controller.home
+import sys
 
 
+# 定义基本信息
 from tornado.options import define,options
 define('port', default=8002, help='give a port!', type=int)
-define('host', default='127.0.01', help='localhost')
+define('host', default='127.0.0.1', help='localhost')
 define('url', default=None, help='The Url Show HTML')
+define('config', default = "./config.yaml", help="config file's full path")
 
 tornado.options.parse_command_line()
 if not tornado.options.options.url:
 	tornado.options.options.url = "http://%s:%d" % (tornado.options.options.host, tornado.options.options.port)
 
+handlers = [
+        (r'/', controller.home.HomeHandler),
+        ]
+settings = {
+    'base_url':options.url,
+    'config_filename':options.config,
+    'cookie_secret': base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes),
+    "xsrf_cookies": True,
+    'template_path': 'templates',
+    'static_path': 'static',
+}
+config = {}
+try:
+    with open(settings["config_filename"], "r") as fin:
+        config = yaml.load(fin)
+    for k, v in config["global"].items():
+        settings[k] = v
+    if "session" in config:
+        settings["session"]["driver_settings"] = config["session"]
+except:
+	print ("cannot found config.yaml file")
+	sys.exit(0)
+
+# 应用程序基础类
 class Application(tornado.web.Application):
     def __init__(self):
-
-        handlers = [
-            (r'/', controller.home.HomeHandler),
-        ]
-
-        settings = {
-            'template_path': 'templates',
-            'static_path': 'static',
-        }
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
--- controller/home.py
+++ controller/home.py
@@ -2,4 +2,13 @@ __author__ = 'zhangxp'
 import tornado.web
 class HomeHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
-        self.render('main.html')
+        #此处从后台数据库获取
+        img_url = '/static/images/logo/header_logo.jpg'
+        img_house = '/static/images/house.jpg'
+        img_news = '/static/images/news.jpg'
+        img_tour = '/static/images/Tourism.jpg'
+        img_chat = '/static/images/Chat.jpg'
+        img_hot = '/static/images/hot.jpg'
+        self.render('main.html',imgurl = img_url,
+                    imghouse = img_house,imgnews = img_news, imgtour = img_tour, imgchat = img_chat,
+                    imghot = img_hot)
